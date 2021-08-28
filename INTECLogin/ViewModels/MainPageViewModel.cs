0540dc4f3bd8ea6191f099521135c867431bd44d
crisog <ortega.cpp@gmail.com>
2021-08-28 03:01:14 -0400

    feat: translate options to english & add different images

--- INTECLogin/ViewModels/MainPageViewModel.cs
+++ INTECLogin/ViewModels/MainPageViewModel.cs
@@ -22,23 +22,23 @@ namespace INTECLogin.ViewModels
 
         public MainPageViewModel()
         {
-            MenuOptionBars.Image = "beerIcon";
-            MenuOptionBars.Title = "Bares y Hoteles";
+            MenuOptionBars.Image = "https://icons.iconarchive.com/icons/flat-icons.com/flat/256/Beer-icon.png";
+            MenuOptionBars.Title = "Bars";
 
-            MenuOptionDining.Image = "fineDiningIcon";
-            MenuOptionDining.Title = "Cenas";
+            MenuOptionDining.Image = "https://icons.iconarchive.com/icons/icons8/windows-8/256/Household-Dining-Room-icon.png";
+            MenuOptionDining.Title = "Dinning";
 
-            MenuOptionCafe.Image = "cafesIcon";
+            MenuOptionCafe.Image = "https://icons.iconarchive.com/icons/iconsmind/outline/256/Coffee-icon.png";
             MenuOptionCafe.Title = "Cafes";
 
-            MenuOptionNearby.Image = "nearbyIcon";
-            MenuOptionNearby.Title = "Cercanos";
+            MenuOptionNearby.Image = "https://icons.iconarchive.com/icons/paomedia/small-n-flat/256/map-marker-icon.png";
+            MenuOptionNearby.Title = "Nearby";
 
-            MenuOptionFastFood.Image = "fastFoodIcon";
-            MenuOptionFastFood.Title = "Comida rápida";
+            MenuOptionFastFood.Image = "https://icons.iconarchive.com/icons/sonya/swarm/256/Fast-Food-icon.png";
+            MenuOptionFastFood.Title = "Fast Food";
             
-            MenuOptionFeaturedFood.Image = "pizzaIcon";
-            MenuOptionFeaturedFood.Title = "Favoritos";
+            MenuOptionFeaturedFood.Image = "https://icons.iconarchive.com/icons/sonya/swarm/256/Pizza-icon.png";
+            MenuOptionFeaturedFood.Title = "Featured";
 
             LoginCommand = new Command<object>((sender) =>
             {
