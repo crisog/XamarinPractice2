08700bfc225828b7e61d05e5066cfddc16e888ac
crisog <ortega.cpp@gmail.com>
2021-08-28 01:24:30 -0400

    feat: add main page bindings

--- INTECLogin/Views/MainPage.xaml.cs
+++ INTECLogin/Views/MainPage.xaml.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using INTECLogin.ViewModels;
 using Xamarin.Forms;
 
 namespace INTECLogin.Views
 {
     public partial class MainPage : ContentPage
     {
+        MenuOptionButtonView lastElementSelected = new MenuOptionButtonView();
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+
+        public void Stack_Clicked(object sender, EventArgs e)
+        {
+            lastElementSelected.BackgroundColor = Color.White;
+            var elementSelected = (MenuOptionButtonView)sender;
+            elementSelected.BackgroundColor = Color.FromHex("#F9A602");
+            lastElementSelected = elementSelected;
         }
     }
 }
