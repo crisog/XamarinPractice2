a8e6e0aefa5d75353823d4da5285417e1723c6f7
crisog <ortega.cpp@gmail.com>
2021-08-27 21:10:56 -0400

    Initial commit, app from practice 1

---
+++ INTECLogin/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace INTECLogin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            if (string.IsNullOrWhiteSpace(emailEntry.Text) || string.IsNullOrWhiteSpace(passwordEntry.Text))
+            {
+                await DisplayAlert("Alerta", "No pueden haber campos vacios!", "OK");
+            } else
+            {
+                await DisplayAlert("Bienvenid@", $"Hola, {emailEntry.Text}", "OK");
+            }
+        }
+    }
+}
