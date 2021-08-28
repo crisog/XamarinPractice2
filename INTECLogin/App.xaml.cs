42afbbd845d5d0785bd5f3b14f7696485ca7edc2
crisog <ortega.cpp@gmail.com>
2021-08-28 01:30:56 -0400

    fix: make main page a navigation page

--- INTECLogin/App.xaml.cs
+++ INTECLogin/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using INTECLogin.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace INTECLogin
         {
             InitializeComponent();
 
-            MainPage = new Views.MainPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
